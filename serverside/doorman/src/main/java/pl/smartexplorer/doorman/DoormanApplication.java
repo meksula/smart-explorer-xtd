880ba50529430814d584b93bceb0d23e7726dd40
meksula <meksula.karol.93@gmail.com>
2018-10-18 23:08:10 +0200

    Doorman init and first html page trial version made

---
+++ serverside/doorman/src/main/java/pl/smartexplorer/doorman/DoormanApplication.java
@@ -0,0 +1,18 @@
+package pl.smartexplorer.doorman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author
+ * Karol Meksuła
+ * 18-10-2018
+ * */
+
+@SpringBootApplication
+public class DoormanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoormanApplication.class, args);
+    }
+}
---
+++ serverside/doorman/src/main/java/pl/smartexplorer/doorman/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package pl.smartexplorer.doorman.security;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author
+ * Karol Meksuła
+ * 30-09-2018
+ * */
+
+@Configuration
+@EnableWebSecurity
+@EnableOAuth2Sso
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/static/**", "/css/**", "/images/**", "/js/**", "/vendor/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+}
