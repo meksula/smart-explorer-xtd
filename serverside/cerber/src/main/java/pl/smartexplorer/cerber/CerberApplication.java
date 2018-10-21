214b546fd2121d24558d022d970768546ae72fe1
meksula <meksula.karol.93@gmail.com>
2018-10-21 21:26:40 +0200

    Cerber microservice generated and Sev2 reorganize

---
+++ serverside/cerber/src/main/java/pl/smartexplorer/cerber/CerberApplication.java
@@ -0,0 +1,18 @@
+package pl.smartexplorer.cerber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author
+ * Karol Meksuła
+ * 21-10-2018
+ * */
+
+@SpringBootApplication
+public class CerberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CerberApplication.class, args);
+    }
+}
---
+++ serverside/cerber/src/test/java/pl/smartexplorer/cerber/CerberApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.smartexplorer.cerber;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CerberApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
