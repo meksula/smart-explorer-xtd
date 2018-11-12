a9efe1ab0c29181ee9d9b3065da4f1ec8df3a07e
meksula <meksula.karol.93@gmail.com>
2018-11-12 13:18:20 +0100

    Login page structure and styled

--- serverside/scribe/src/main/java/pl/smartexplorer/scribe/services/mailer/MailerController.java
+++ serverside/scribe/src/main/java/pl/smartexplorer/scribe/services/mailer/MailerController.java
@@ -1,6 +1,5 @@
 package pl.smartexplorer.scribe.services.mailer;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
